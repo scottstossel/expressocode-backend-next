584b87266a4ce17ea50b6825ab152f2f65233265
Scott Stossel <stoss96@gmail.com>
2021-11-03 23:01:41 -0400

    blog list, get single, and delete added

--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,4 +1,6 @@
 const Blog = require('../models/Blog');
+const Category = require('../models/Category');
+const Tag = require('../models/Tag');
 const formidable = require('formidable');
 const slugify = require('slugify');
 const {stripHtml} = require('string-strip-html');
@@ -99,3 +101,99 @@ exports.create = (req, res) => {
       });
   });
 };
+
+//list, listblogs..., read, remove, update
+exports.list = (req, res) => {
+    Blog.find({})
+    .populate('categories', '_id name slug')
+    .populate('tags', '_id name slug')
+    .populate('postedBy', '_id name username')
+    .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+    .exec((err, data) => {
+        if (err) {
+            return res.json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+}
+
+exports.listAllBlogsCategoriesTags = (req, res) => {
+    let limit = req.body.limit ? parseInt(req.body.limit) : 10;
+    let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
+    let blogs
+    let categories
+    let tags
+
+    Blog.find({})
+    .populate('categories', '_id name slug')
+        .populate('tags', '_id name slug')
+        .populate('postedBy', '_id name username profile')
+        .sort({createdAt: -1})
+        .skip(skip)
+        .limit(limit)
+        .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+        .exec((err, data) => {
+            if (err) {
+                return res.json({
+                    error: errorHandler(err)
+                });
+            }
+            blogs = data
+            //get all categories
+            Category.find({}).exec((err, c) => {
+                if (err) {
+                    return res.json({
+                        error: errorHandler(err)
+                    });
+                }
+                categories = c;
+                //get all tags
+                Tag.find({}).exec((err, t) => {
+                    if (err) {
+                        return res.json({
+                            error: errorHandler(err)
+                        });
+                    }
+                    tags = t;
+                    //return all blogs, categories, and tags
+                    res.json({blogs, categories, tags, size: blogs.length});
+                })
+            })
+        })
+}
+
+exports.read = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOne({slug})
+    .populate('categories', '_id name slug')
+        .populate('tags', '_id name slug')
+        .populate('postedBy', '_id name username')
+        .select('_id title body slug mtitle mdesc categories tags postedBy createdAt updatedAt')
+        .exec((err, data) => {
+            if (err) {
+                return res.json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(data);
+        })
+}
+exports.remove = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+    Blog.findOneAndRemove({slug}).exec((err, data) => {
+        if (err) {
+            return res.json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Blog deleted successfully'
+        })
+    })
+}
+exports.update = (req, res) => {
+    //
+}
--- routes/blog.js
+++ routes/blog.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {create} = require('../controllers/blogControllers.js');
+const {create, list, listAllBlogsCategoriesTags, read, remove, update} = require('../controllers/blogControllers.js');
 const {requireSignin, adminMiddleware} = require('../controllers/authControllers');
 
 router.post('/blog', requireSignin, adminMiddleware, create);
+router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags); //improves SEO
+router.get('/blog/:slug', read);
+router.delete('/blog/:slug', requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug', requireSignin, adminMiddleware, update);
 
 module.exports = router;
